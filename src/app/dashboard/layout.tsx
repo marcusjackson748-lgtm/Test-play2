904532f9cc7164f5d02bdc86ca478f7225bdcd8e
marcusjackson748-lgtm <marcusjackson748@gmail.com>
2026-07-29 22:53:30 +0100

    Change background color class in layout component

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -11,7 +11,7 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="relative flex flex-col min-h-full bg-[#050607] text-white">
+    <div className="relative flex flex-col min-h-full bg-brandBg text-white">
       {/* Base vignette for cinematic depth */}
       <div className="pointer-events-none fixed inset-0 -z-10 bg-[radial-gradient(ellipse_at_top,_rgba(142,240,138,0.04),_transparent_60%)]" />
       <div className="pointer-events-none fixed inset-0 -z-10 bg-[linear-gradient(180deg,_rgba(0,0,0,0)_0%,_rgba(0,0,0,0.4)_100%)]" />
